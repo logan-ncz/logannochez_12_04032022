5a69f576da9124402157c33a181e2d6bb4f263a8
Logan Nochez <logannochez@hotmail.fr>
2022-03-31 18:54:16 +0200

    style

--- src/components/Charts/Score.jsx
+++ src/components/Charts/Score.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { PieChart, Pie, Cell, Label } from 'recharts';
+import { PieChart, Pie, Cell } from 'recharts';
 import { useFetch } from '../../utils/Fetch'
 
 
@@ -21,11 +21,13 @@ export default function Score(props) {
                     <Pie data={data01} nameKey="score" dataKey="value" cx="50%" cy="50%" innerRadius={90} outerRadius={100} cornerRadius={40} paddingAngle={2} startAngle={90}>
                         <Cell fill='#FF0000'/>
                         <Cell fill='rgba(0, 0, 0, 0)' stroke='#FBFBFB'/>
-                        <Label className='score_label_percent' position="center">
-                            {`${data01[0].value}%`}
-                        </Label>
                     </Pie>
+                    <Pie data={data01} dataKey="value" cx="50%" cy="50%" outerRadius={90} fill="#ffffff" />
                 </PieChart>
+                <p className='score_label_percent'>
+                    {`${data01[0].value}%`}
+                </p>
+                <p className='score_text'>de votre <br />objectif</p>
             </div>
         );
     }
